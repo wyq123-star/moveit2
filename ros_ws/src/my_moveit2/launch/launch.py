34f777606a5b2ec5b1d540bb996f0ad31a9b4ff7
yuqin wang <3186831775@qq.com>
2025-02-20 18:22:59 +0800

    加入了自己编写的节点文件

--- ros_ws/src/my_moveit2/launch/launch.py
+++ ros_ws/src/my_moveit2/launch/launch.py
@@ -5,7 +5,14 @@ from ament_index_python.packages import get_package_share_directory
 from os.path import join
 
 def generate_launch_description():
-    # 只包含 assistant 包中的 demo.launch.py 文件
+    # 包含 my_moveit2 包中的 launch 文件
+    my_moveit2_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            join(get_package_share_directory('my_moveit2'), 'launch', 'launch.py')  # 这里的 launch.py 需要是你的 my_moveit2 包中的启动文件
+        )
+    )
+    
+    # 包含 assistant 包中的 demo.launch.py 文件
     assistant_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             join(get_package_share_directory('assistant'), 'launch', 'demo.launch.py')  # 这里的 demo.launch.py 需要是你的 assistant 包中的启动文件
@@ -13,5 +20,6 @@ def generate_launch_description():
     )
 
     return LaunchDescription([
-        assistant_launch  # 只返回 assistant_launch
+        my_moveit2_launch,
+        assistant_launch  # 这里我们使用 assistant_launch
     ])
